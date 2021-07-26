24d86f02e5ad5f892d4f3dbf488d021b3f14abf9
Abdou9696 <abdou.trichili@gmail.com>
2021-07-26 03:59:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from flask import Flask, redirect, url_for, request, render_template, flash
+from flask import Flask, redirect, url_for, request, render_template
 
 import requests
 import json
 
-from jinja2 import Markup
-
+# Initialize the Key and the endpoint
 API_ENDPOINT = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?region=US&lang=en"
 API_KEY = "ba1d2f78e2msh289a19bec03e479p161cafjsn1315cd23cfde"
 
-
-
-
-
-
 def get_data(symbol):
+    """
+    Function to get the ticker data from Yahoo finance API
+
+    :param symbol: the ticker symbol (string)
+    :return: dict which contains data about the ticker
+    """
     response = requests.get(
         API_ENDPOINT,
         headers={'X-RapidAPI-Key': API_KEY},
@@ -24,43 +24,58 @@ def get_data(symbol):
     else:
         return ("Error retrieving API data for " + symbol)
 
-
-
 def get_price(data):
+    """
+    Function to get the regular Market Previous Close of the ticker
+
+    :param data: dict which contains data about the ticker
+    :return: the regular Market Previous Close (float)
+    """
     return data["summaryDetail"]["regularMarketPreviousClose"]["raw"]
 
 
 app = Flask(__name__)
 
 
-@app.route('/<name>/<price>/<pos>/<notional>')
-def home(name,price,pos,notional):
 
-    return render_template('rslt.html',name=name, price=price ,pos =pos, notional=notional)
 
 
 @app.route('/',methods = ['POST', 'GET'])
-def l():
+def input():
     error = None
     if request.method == 'POST':
-
-        symbol = (request.form['symbol'])
+        #get the ticker symbolr from the input and use it to get data
+        symbol = request.form['symbol']
         data=get_data(symbol)
+        #get the position from the input which will be used in calculation
+        quantity = int(request.form['qte'])
+
+        # if ticker symbol didn't exist, an error message will display
         if data =={}:
             error = 'Invalid ticker'
             render_template('index.html', error=error)
+
         else:
-            quantity = int(request.form['qte'])
+            # if quantity is < 0, an error message will display (but it's impossible to reach this case because
+            # conditions for the insert number were added in the html page (it should be a number and > 0,
+            # or the submission will not be done. By dafault the position is 1
             if quantity < 0:
                 error = 'Quantity must be > 0'
                 render_template('index.html', error=error)
+
             else:
                 price = get_price(data)
                 rslt = quantity * price
-                return redirect(url_for('home', name=symbol, price=price, pos =quantity, notional=rslt))
+
+                #redirection for the page which will show the result
+                return redirect(url_for('rslt', name=symbol.upper(), price=price, pos =quantity, notional=rslt))
 
     return render_template('index.html',error =error )
 
+#The redirected page to show the result
+@app.route('/<name>/<price>/<pos>/<notional>')
+def rslt(name,price,pos,notional):
+    return render_template('rslt.html',name=name, price=(price) ,pos =pos, notional=notional)
 
 
 if __name__ == '__main__':
